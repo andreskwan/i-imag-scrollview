bd957059ab840a0d3639bb57167c8f13ac6248c6
ANDRES KWAN <andreskwan@gmail.com>
2014-11-25 16:58:02 -0500

    To understan Multithreading - UIScrollView

---
+++ ios7-v10-multithreads-uiscrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios7-v10-multithreads-uiscrollview
+//
+//  Created by Andres Kwan on 11/25/14.
+//  Copyright (c) 2014 Kwan Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios7-v10-multithreads-uiscrollview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios7-v10-multithreads-uiscrollview
+//
+//  Created by Andres Kwan on 11/25/14.
+//  Copyright (c) 2014 Kwan Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
