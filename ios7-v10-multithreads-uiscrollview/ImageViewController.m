4e092678fa7a72e925a0e7990cebf82a6ae92ff8
ANDRES KWAN <andreskwan@gmail.com>
2014-12-01 19:02:06 -0500

    v10.6.6 - add UIScrollView - pending Zoom

--- ios7-v10-multithreads-uiscrollview/ImageViewController.m
+++ ios7-v10-multithreads-uiscrollview/ImageViewController.m
@@ -28,7 +28,9 @@
 {
     _imageURL = imageURL;
     //if this is URL in the internet is going to block the main queue!
-    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    self.image =
+    [UIImage imageWithData:
+     [NSData dataWithContentsOfURL:self.imageURL]];
 }
 
 //we dont use a instance variable to store it!
@@ -45,8 +47,21 @@
     //
     self.imageView.image = image;
     [self.imageView sizeToFit];
+    //any time we change the image we need to set the content size
+    //if self.image is nil, this might cause problems
+    self.scrollView.contentSize =
+        self.image ? self.image.size : CGSizeZero;
+
 }
 
+//could be set after setImage!!!
+//prepare for segue
+//is call before the outlets are set!!!
+- (void)setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+}
 
 #pragma mark - Life Cycle methods
 -(void)viewDidLoad
@@ -55,6 +70,6 @@
     //but this means that we need
     //self.imageView lazy instantiated
     //if this variable at this stage is empty
-    [self.view addSubview:self.imageView];
+    [self.scrollView  addSubview:self.imageView];
 }
 @end
